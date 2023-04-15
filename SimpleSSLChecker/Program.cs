6b1b76e6faa121c9fd3da2c5a675153a67af334e
Anton Savostin <asavostin23@gmail.com>
2023-04-15 22:08:55 +0300

    Parrallel version (?)

--- SimpleSSLChecker/Program.cs
+++ SimpleSSLChecker/Program.cs
@@ -8,38 +8,18 @@ using (StreamReader reader = new("..\\..\\...\\..\\..\\input.txt"))
     while ((line = await reader.ReadLineAsync()) != null)
         uris.Add(line);
 }
-
-DateTime dateTimeBefore = DateTime.Now;
-
-StreamWriter writer = new StreamWriter("..\\..\\...\\..\\..\\output.txt");
-await writer.WriteLineAsync(dateTimeBefore.ToString());
-
 FixUris(uris);
 
-int counter = 1;
-foreach (string uri in uris)
+StreamWriter writer = new StreamWriter("..\\..\\...\\..\\..\\output.txt");
+await writer.WriteLineAsync(DateTime.Now.ToString());
+
+int threads = 4;
+Task[] tasks = new Task[threads];
+for (int i = 0; i < threads; i++)
 {
-    CertificateData? certData = await CertificateData.GetCertData(uri);
-
-    StringBuilder result = new StringBuilder();
-    DateTime expirationDateTime;
-
-    if (certData != null && DateTime.TryParse(certData.ExpirationDateString, out expirationDateTime))
-    {
-        if (expirationDateTime.Subtract(DateTime.Now).TotalDays > 0)
-            result.Append($"{counter++} - {uri} - VALID - {((int)expirationDateTime.Subtract(DateTime.Now).TotalDays)} days left");
-        else
-            result.Append($"{counter++} - {uri} - EXPIRED  - {((int)expirationDateTime.Subtract(DateTime.Now).TotalDays)} days left");
-    }
-    else
-        result.Append($"{counter++} - {uri} - FAIL");
-
-    await writer.WriteLineAsync(result.ToString());
+    tasks[i] = ProcessSites(uris.GetRange(i * uris.Count / threads, uris.Count / threads), writer);
 }
-DateTime dateTimeAfter = DateTime.Now;
-
-await writer.WriteLineAsync($"Time estimated: {(uint)dateTimeAfter.Subtract(dateTimeBefore).TotalSeconds} seconds and {counter - 1} sites were checked");
-
+await Task.WhenAll(tasks);
 writer.Close();
 static void FixUris(IList<string> uris)
 {
@@ -51,4 +31,31 @@ static void FixUris(IList<string> uris)
             else
                 uris[i] = "https://" + uris[i];
     }
+}
+static async Task ProcessSites(IEnumerable<string> uris, StreamWriter writer)
+{
+    DateTime dateTimeBefore = DateTime.Now;
+    int counter = 1;
+    foreach (string uri in uris)
+    {
+        CertificateData? certData = await CertificateData.GetCertData(uri);
+
+        StringBuilder result = new StringBuilder();
+        DateTime expirationDateTime;
+
+        if (certData != null && DateTime.TryParse(certData.ExpirationDateString, out expirationDateTime))
+        {
+            if (expirationDateTime.Subtract(DateTime.Now).TotalDays > 0)
+                result.Append($"{counter++} - {uri} - VALID - {((int)expirationDateTime.Subtract(DateTime.Now).TotalDays)} days left");
+            else
+                result.Append($"{counter++} - {uri} - EXPIRED  - {((int)expirationDateTime.Subtract(DateTime.Now).TotalDays)} days left");
+        }
+        else
+            result.Append($"{counter++} - {uri} - FAIL");
+
+        await writer.WriteLineAsync(result.ToString());
+    }
+    DateTime dateTimeAfter = DateTime.Now;
+
+    await writer.WriteLineAsync($"Time estimated: {(uint)dateTimeAfter.Subtract(dateTimeBefore).TotalSeconds} seconds and {counter - 1} sites were checked");
 }
