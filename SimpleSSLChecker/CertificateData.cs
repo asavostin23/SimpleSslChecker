410a7b27744ce740005ad6b023bd1f3a5554c07e
Anton Savostin <asavostin23@gmail.com>
2023-04-15 20:58:42 +0300

    Separated CertificateData class. Changes of error handling

---
+++ SimpleSSLChecker/CertificateData.cs
@@ -0,0 +1,62 @@
+﻿using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+
+namespace SimpleSSLChecker
+{
+    public class CertificateData
+    {
+        public async static Task<CertificateData?> GetCertData(string uri)
+        {
+            CertificateData? certData = null;
+
+            HttpClientHandler handler = new();
+            handler.ServerCertificateCustomValidationCallback = ServerCertificateCustomValidation;
+            HttpClient client = new HttpClient(handler);
+
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(uri);
+                response.EnsureSuccessStatusCode();
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine($"Exception {uri} -  {e.Message}");
+                
+                return null;
+            }
+            finally
+            {
+                handler.Dispose();
+                client.Dispose();
+            }
+            return certData;
+
+            bool ServerCertificateCustomValidation(HttpRequestMessage requestMessage, X509Certificate2? certificate, X509Chain? chain, SslPolicyErrors sslErrors)
+            {
+                if (certificate is not null)
+                    certData = new CertificateData(uri, certificate);
+                return sslErrors == SslPolicyErrors.None;
+            }
+        }
+
+        public string URI { get; } = "unset";
+        public string EffectiveDateString { get; }
+        public string ExpirationDateString { get; }
+
+        public string Issuer { get; }
+
+        public string Subject { get; }
+
+        public CertificateData(X509Certificate2 cert)
+        {
+            EffectiveDateString = cert.GetEffectiveDateString();
+            ExpirationDateString = cert.GetExpirationDateString();
+            Issuer = cert.Issuer;
+            Subject = cert.Subject;
+        }
+        public CertificateData(string uri, X509Certificate2 cert) : this(cert)
+        {
+            URI = uri;
+        }
+    }
+}
--- SimpleSSLChecker/Program.cs
+++ SimpleSSLChecker/Program.cs
@@ -1,102 +1,53 @@
-﻿using System.Net.Security;
-using System.Security.Cryptography.X509Certificates;
-using static System.Net.WebRequestMethods;
+﻿using SimpleSSLChecker;
 
 string[] uris = new string[]
 {
     "https://docs.microsoft.com/",
+    "byfly.by",
     "https://microsoft.com/",
+    "metanit.com",
     "google.com",
-    "advansys.com"
-
+    "advansys.com",
+    "vk.com",
+    "facebook.com",
+    "instagram.com",
+    "drive.google.com"
 };
 
-for (int i = 0; i < uris.Length; i++)
-{
-    if (!uris[i].StartsWith("https://"))
-        if (uris[i].StartsWith("http://"))
-            uris[i].Replace("http://", "https://");
-        else
-            uris[i] = "https://" + uris[i];
-}
+FixUris(uris);
 
 DateTime dateTimeBefore = DateTime.Now;
-
 int counter = 1;
 foreach (string uri in uris)
 {
-    CertificateData? certData = await GetCertData(uri);
+    CertificateData? certData = await CertificateData.GetCertData(uri);
+    Console.WriteLine($"Site: {counter++}");
     if (certData != null)
     {
-        Console.WriteLine($"Site: {counter++}");
         Console.WriteLine($"URI: {certData.URI}");
-        Console.WriteLine($"Effective date: {certData.EffectiveDateString}");
+        //Console.WriteLine($"Effective date: {certData.EffectiveDateString}");
         Console.WriteLine($"Exp date: {certData.ExpirationDateString}");
-        Console.WriteLine($"Issuer: {certData.Issuer}");
-        Console.WriteLine($"Subject: {certData.Subject}");
+        //Console.WriteLine($"Issuer: {certData.Issuer}");
+        //Console.WriteLine($"Subject: {certData.Subject}");
         Console.WriteLine();
     }
     else
     {
-        Console.WriteLine($"Certificate for URI {uri} was null");
+        Console.WriteLine($"URI: {uri} - FAIL\n");
     }
 }
-
 DateTime dateTimeAfter = DateTime.Now;
-Console.WriteLine($"\nTime estimated: {(uint)dateTimeAfter.Subtract(dateTimeBefore).TotalSeconds} seconds and {counter} sites were checked");
-async static Task<CertificateData?> GetCertData(string uri)
+
+Console.WriteLine($"\nTime estimated: {(uint)dateTimeAfter.Subtract(dateTimeBefore).TotalSeconds} seconds and {counter - 1} sites were checked");
+
+static void FixUris(string[] uris)
 {
-    CertificateData? certData = null;
-
-    HttpClientHandler handler = new();
-    handler.ServerCertificateCustomValidationCallback = ServerCertificateCustomValidation;
-    HttpClient client = new HttpClient(handler);
-
-    try
+    for (int i = 0; i < uris.Length; i++)
     {
-        HttpResponseMessage response = await client.GetAsync(uri);
-        response.EnsureSuccessStatusCode();
-
-        string responseBody = await response.Content.ReadAsStringAsync();
-    }
-    catch (HttpRequestException e)
-    {
-        Console.WriteLine("\nException Caught!");
-        Console.WriteLine($"Message: {e.Message}");
-    }
-    finally
-    {
-        handler.Dispose();
-        client.Dispose();
-    }
-    return certData;
-
-    bool ServerCertificateCustomValidation(HttpRequestMessage requestMessage, X509Certificate2? certificate, X509Chain? chain, SslPolicyErrors sslErrors)
-    {
-        if (certificate is not null)
-            certData = new CertificateData(uri, certificate);
-        return sslErrors == SslPolicyErrors.None;
-    }
-}
-public class CertificateData
-{
-    public string URI { get; } = "unset";
-    public string EffectiveDateString { get; }
-    public string ExpirationDateString { get; }
-
-    public string Issuer { get; }
-
-    public string Subject { get; }
-
-    public CertificateData(X509Certificate2 cert)
-    {
-        EffectiveDateString = cert.GetEffectiveDateString();
-        ExpirationDateString = cert.GetExpirationDateString();
-        Issuer = cert.Issuer;
-        Subject = cert.Subject;
-    }
-    public CertificateData(string uri, X509Certificate2 cert) : this(cert)
-    {
-        URI = uri;
+        if (!uris[i].StartsWith("https://"))
+            if (uris[i].StartsWith("http://"))
+                uris[i].Replace("http://", "https://");
+            else
+                uris[i] = "https://" + uris[i];
     }
 }
